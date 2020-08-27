3e140f9151d7798e8ac422cbd4d0a1d456ad7897
Michael Lamb <mflambjr@gmail.com>
2020-08-26 21:19:37 -0400

    configure critical css and source-filesystem plugins

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,8 +7,31 @@
 module.exports = {
   siteName: `Dar's Steaks`,
   siteDescription: `Serving the World's Best Cheesesteaks in Haddon Township, NJ`,
-  plugins: [],
+  permalinks: {
+    trailingSlash: true
+  },
   icon: './uploads/dars-steaks-serif.png',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'markdowns/menu/**/*.md',
+        typeName: 'MenuContent',
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksRel: ['nofollow', 'noopener', 'noreferrer']
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-critical',
+      options: {
+        paths: ['/'],
+        width: 1300,
+        height: 900
+      }
+    }
+  ],
   chainWebpack: config => {
     config.module
       .rule('pug')
