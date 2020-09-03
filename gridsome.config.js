16d8005a8d8583262f0b5366d3c7b186fc8f33b1
Michael Lamb <mflambjr@gmail.com>
2020-09-03 03:02:20 -0400

    un-comment global siteDescription

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   siteName: `Dar's Steaks`,
-  // siteDescription: `Serving the World's Best Cheesesteaks in Westmont, NJ`,
+  siteDescription: `Serving the World's Best Cheesesteaks in Westmont, NJ`,
   titleTemplate: `%s | Dar's Steaks`,
   siteUrl: 'https://www.darssteaks.com',
   permalinks: {
