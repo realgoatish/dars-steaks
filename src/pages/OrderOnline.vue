35104102393e7271f41bea9c0528310db74d5561
Mike Lamb <mflambjr@gmail.com>
2023-10-31 15:15:55 -0400

    add order online page

---
+++ src/pages/OrderOnline.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  Layout
+    main
+      div(class="center")
+        div(class="stack")
+          div
+            h1 Order Online
+          div(class="cluster")
+            a(class="order-online-link" href="https://www.clover.com/online-ordering/dars-steaks") Order Pickup
+            a(class="order-online-link" href="https://www.grubhub.com/restaurant/dars-steaks-142-haddon-ave-collingswood/6837120") Order Delivery
+
+</template>
+
+<style scoped>
+
+  main {
+    height: 100%;
+    padding-block-start: 4rem;
+    padding-block-end: 10rem;
+    font-size: 120%;
+  }
+
+  div {
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .center {
+    box-sizing: content-box;
+    margin-inline: auto;
+  }
+
+  .stack {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start
+  }
+
+  .stack > * {
+    margin-block: 0;
+  }
+
+  .stack > * + * {
+    margin-block-start: 2rem;
+  }
+
+  .cluster {
+    display: flex;
+		flex-wrap: wrap;
+		gap: var(--space, 1rem);
+		justify-content: center;
+		align-items: center;
+  }
+
+</style>
